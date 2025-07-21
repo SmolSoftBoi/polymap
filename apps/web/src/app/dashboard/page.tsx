8d5ef38519823ab5b736456e52d65a19b4538035
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2025-07-21 21:24:45 +0100

    feat: display user email on dashboard

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next'
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { requireAuthentication } from '@/utils/auth'
 
 /** Metadata for the dashboard page. */
@@ -10,9 +12,15 @@ export const metadata: Metadata = {
 /** Page displaying the user dashboard. */
 export default async function DashboardPage() {
   await requireAuthentication()
+  const supabase = createServerComponentClient({ cookies })
+  const {
+    data: { user }
+  } = await supabase.auth.getUser()
+  const email = user?.email ?? null
   return (
-    <main className="grid flex-1 place-content-center p-8">
+    <main className="grid flex-1 place-content-center space-y-2 p-8">
       <h1 className="text-2xl font-bold">Dashboard 🗺️</h1>
+      {email && <p>Signed in as {email}</p>}
     </main>
   )
 }
